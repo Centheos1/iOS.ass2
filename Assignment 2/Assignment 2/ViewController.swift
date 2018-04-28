afffa9c87c05d52c015f5917d81e2c4de181182d
Centheos <cesellen@gmail.com>
2018-04-28 20:07:58 +1000

    created xcode project

---
+++ Assignment 2/Assignment 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Clint Sellen on 28/4/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
